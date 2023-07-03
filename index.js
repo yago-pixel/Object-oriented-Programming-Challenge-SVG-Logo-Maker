1931405cc5ae4ce083d7b7661bedf55aa817676a
Yagol <yagolira02@hotmail.com>
2023-07-03 11:50:31 -0400

    added updating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Importing classes from ./lib/shapes directory
-const { Triangle, Square, Circle } = require("./lib/shapes");
+const { Triangle, Square, Circle } = require("./index.js");
 
 // Function writes the SVG file using user answers from inquirer prompts
 function writeToFile(fileName, answers) {
