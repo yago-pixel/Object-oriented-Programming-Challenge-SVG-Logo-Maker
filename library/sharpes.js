e952bfaa3ae6830323120b01e8c7b280a903a857
Yagol <yagolira02@hotmail.com>
2023-07-03 23:12:02 -0400

    added updating sharpes.js

--- library/sharpes.js
+++ library/sharpes.js
@@ -1,26 +1,24 @@
-class Shape{
+const filesystem = require('./filesystem')
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./index.js");
+// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
+// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
+
+class Svg{
     constructor(){
-}
-
-setColor(){
-
-}
-}
-
-class Circle extends Shape{
+        this.textElement = ''
+        this.shapeElement = ''
+    }
     render(){
 
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
     }
-}
-
-class Triangle extends Shape{
-    render(){
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
 
     }
-}
-
-class Square extends Shape{
-    render(){
-        
-    }
+    
 }
