fdaba170f58045d91c9501eb1c08660a6fa518f1
Yagol <yagolira02@hotmail.com>
2023-07-03 23:02:34 -0400

    added updating README.md

---
+++ library/sharpes.js
@@ -0,0 +1,26 @@
+class Shape{
+    constructor(){
+}
+
+setColor(){
+
+}
+}
+
+class Circle extends Shape{
+    render(){
+
+    }
+}
+
+class Triangle extends Shape{
+    render(){
+
+    }
+}
+
+class Square extends Shape{
+    render(){
+        
+    }
+}
