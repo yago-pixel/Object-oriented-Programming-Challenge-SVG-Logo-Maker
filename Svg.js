904a31416cfd90d7efdea1603b4f045203c8e644
yago-pixel <yagolira02@hotmail.com>
2023-09-24 01:57:52 -0400

    updating index.js

---
+++ Svg.js
@@ -0,0 +1,21 @@
+// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
+// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
+class Svg {
+  constructor() {
+    this.textElement = '';
+    this.shapeElement = '';
+  }
+  render() {
+
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+  }
+  setTextElement(text, color) {
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
+  setShapeElement(shape) {
+    this.shapeElement = shape.render();
+
+  }
+
+}
+exports.Svg = Svg;
--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 const filesystem = require('./Users/yagol/OneDrive/Área de Trabalho/assignments/Object-oriented-Programming-Challenge-SVG-Logo-Maker/.git/')
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shapes");
-// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
-// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
-
-class Svg{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render(){
-
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    }
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
-
-    }
-    
-}
-
+const { Svg } = require('./Svg');
 // Defines array of 'questions' using the 'inquirer' library with the following questions.
 // Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
 const questions = [
